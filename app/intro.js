4f38c77ee527613eb6af9e90ac10833eafcd2a20
pokorra <patrycja.pokora@gmail.com>
2021-02-12 23:33:28 +0100

    basic setup of main page, styled header of main page

--- app/intro.js
+++ app/intro.js
@@ -10,13 +10,13 @@ const stamp = document.querySelector('.stamp');
 
 const timeline = new TimelineMax();
 
-timeline.fromTo(halfFirst, 1, {height: "0%"}, {height: "100%", ease: Power2.linear})
-    .fromTo(halfSecond, 1, {height: "0%"}, {height: "100%", ease: Power2.easeIn})
-    .fromTo(siteTitle, 2, {opacity: "0"}, {opacity: "1"})
-    .fromTo(secondSection, 1.5, {left: "-100vw"}, {left: "0vw"})
+timeline.fromTo(halfFirst, 1, {height: "0%"}, {height: "100%", ease: Power0.easeNone})
+    .fromTo(halfSecond, 1, {height: "0%"}, {height: "100%", ease: Power0.easeNone})
+    .fromTo(siteTitle, 2, {opacity: "1"}, {opacity: "1"})
+    .fromTo(secondSection, 1.5, {left: "-100vw"}, {left: "0vw" , ease: Power0.easeNone})
     .to(secondSection, 5, {opacity: "1"})
     .fromTo(stamp, .5, {display: "none"}, {display: "block"})
-    .to(stamp, 2, {display: "block"})
-    .fromTo(thirdSection, .5, {opacity: "0"}, {opacity: "1"})
-    .fromTo(stamp, .5, {display: "block"}, {display: "none"}, "-=.5")
-    .fromTo(secondSection, 1.5, {right: "0vw"}, {right: "100vw"}, "-=.5")
+    .to(stamp, 2, {display: "block"})    
+    .to(stamp, .5, {display: "none"})
+    .fromTo(secondSection, 1.5, {right: "0vw"}, {right: "100vw"}, "-=1")
+    .fromTo(thirdSection, .5, {opacity: "0"}, {opacity: "1"} , "-=.5")
