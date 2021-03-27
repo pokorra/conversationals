dd0ea118a7e5d7ec40f71262359e97f460beb8f1
pokorra <patrycja.pokora@gmail.com>
2021-03-27 18:06:01 +0100

    replacing for loop with forEach array method in about.js file

--- app/about.js
+++ app/about.js
@@ -1,13 +1,11 @@
 const openingButtons = document.querySelectorAll(".collapse");
 
-for (let i = 0; i < openingButtons.length; i++) {
-    openingButtons[i].addEventListener("click", function () {
+openingButtons.forEach((openingButton) => {
+    openingButton.addEventListener("click", function () {
         this.classList.toggle("about-active");
         let content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
+        content.style.maxHeight
+            ? (content.style.maxHeight = null)
+            : (content.style.maxHeight = content.scrollHeight + "px");
     });
-}
+});
